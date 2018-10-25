55324691ea23be8ddd86de9b7478ac6c07650550
weiyalin <727299708@qq.com>
2018-10-25 18:05:15 +0800

    'test'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -115,7 +115,7 @@ class UserController extends Controller
      */
     public function doFollow($id)
     {
-        $user = $this->user->getById($id);
+        $user = User::query()->findOrFail($id);
 
         if (Auth::user()->isFollowing($id)) {
             Auth::user()->unfollow($id);
